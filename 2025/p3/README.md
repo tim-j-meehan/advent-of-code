acb34bc35b27b6717184c8b54f424e4264c8675e
Meehan, Tim (Contractor, DeVine Consultants) <tim.meehan.ctr@nps.edu>
2026-01-04 12:06:20 -0800

    p3

---
+++ 2025/p3/README.md
@@ -0,0 +1,3 @@
+Strategy for part one is pretty basic.  Just grab the max val in the string not includeing the ffinal val.  Then grab the max val from the location of this max to the end of the string
+
+Part 2 takes the same basic approach but requires extra book keeping to make sure we save off enough digits to complete the sequence.  
---
+++ 2025/p3/p3a.py
@@ -0,0 +1,18 @@
+import sys
+import numpy as np
+from itertools import combinations
+sys.path.append("../")
+import aoclib
+
+fp = open(sys.argv[1])
+cnt = 0
+for line in fp.readlines():
+    vals = [int(i) for  i in line[0:-1]]
+    maxv = max(vals[0:-1])
+    maxi = vals[0:-1].index(maxv)
+    maxv2 = max(vals[maxi+1:])
+    cnt += maxv*10 + maxv2
+    print(maxv,maxv2) 
+    print( vals)
+print(cnt)
+    
