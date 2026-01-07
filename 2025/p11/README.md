74108c7e7ee0f3a05ee1c66dd77f162bf0ea4e3a
Meehan, Tim (Contractor, DeVine Consultants) <tim.meehan.ctr@nps.edu>
2026-01-06 17:55:59 -0800

    cleanup

--- 2025/p11/README.md
+++ 2025/p11/README.md
@@ -1,4 +1,7 @@
-first was very easy iwht NetworkX
+First was very easy wiht NetworkX, required reading the data
+into a diGraph and then calling all_simple_paths()
 
 second part was intersesting.  I first tried to solve
 with networkX but ended up using recursions with lru_cache in the end
+for a very clean and fast solution
+
--- 2025/p11/p11b.py
+++ 2025/p11/p11b.py
@@ -2,7 +2,7 @@ import sys
 from scipy.signal import convolve2d
 import numpy as np
 from itertools import combinations
-sys.path.append("../")
+sys.path.append("../../pylib")
 import aoclib
 import copy
 import pprint
@@ -11,16 +11,8 @@ import matplotlib.pyplot as plt
 from functools import lru_cache
 cnt = 0
 
-def invalid(foo):
-    N = len(foo)
-    if foo[0:N//2] == foo[N//2:N]:
-        return int(foo)
-    return(0)
 fp = open(sys.argv[1])
 
-
-
-first = True 
 vals = []
 cnt = 0
 doit = False
@@ -61,8 +53,6 @@ def find_num_paths(G,stt,stp):
     return sum(
         [find_num_paths(G,stt,ni) for ni,no in G.in_edges(stp)]
     )
-        
-
 print(find_num_paths(G,'svr','out'))
 
 a=find_num_paths(G,'svr','fft')
@@ -74,173 +64,3 @@ f=find_num_paths(G,'dac','fft')
 g=find_num_paths(G,'fft','out')
 
 print("final",a*b*c+e*f*g)
-import pdb
-pdb.set_trace()
-
-if True:
-    #for pth in nx.all_simple_paths(G,"dac","out"):
-#    for pth in nx.all_simple_paths(G,"out","dac"):
-#        cnt3 +=1
-#    print("c",cnt3)
-
-
-    #for pth in nx.all_simple_paths(G,"fft","dac"):
-    for pth in nx.all_simple_paths(G,"dac","fft"):
-        cnt2 +=1
-    print("b",cnt2)
-
-
-    print(nx.has_path(G,"svr","fft"))
-    #for pth in nx.all_simple_paths(G,"svr","fft"):
-    for pth in nx.all_simple_paths(G,"fft","svr"):
-        cnt1 +=1
-        print(cnt1)
-    print("a",cnt1)
-
-    tst1 = cnt1*cnt2*cnt3
-    cnt1=0
-    cnt2=0
-    cnt3=0
-
-    for pth in nx.all_simple_paths(G,"svr","dac"):
-        cnt1 +=1
-    print("a",cnt1)
-    for pth in nx.all_simple_paths(G,"dac","fft"):
-        cnt2 +=1
-    print("b",cnt2)
-    for pth in nx.all_simple_paths(G,"fft","out"):
-        cnt3 +=1
-    print("c",cnt3)
-
-    tst2 = cnt1*cnt2*cnt3
-
-
-print("SUM",tst1,tst2,tst1+tst2)
-
-
-
-
-if False:
-    nx.draw(G,with_labels=True)
-    plt.show()
-    print(G)       
-    print("CNT IS",cnt)    
-
-    lights = vals[0][1:-1]
-    #light = sum([1<<k if v == '#' else 0 for k,v in enumerate(lights[::-1])]) 
-    light = sum([1<<k if v == '#' else 0 for k,v in enumerate(lights)]) 
-    joltage = vals[-1]
-    buttons = [sum([1<<int(x) for x in v[1:-1].split(',')]) for v in vals[1:-1]]
-    
-    
-    print("lights",lights)
-    print("light",light)
-    print("joltage",joltage)
-    print("buttons",buttons)
-    
-    
-    mincnt = 9999 
-    for ii in range(2**len(buttons)):
-        tst = 0
-        cnt = 0
-        print("next")
-        for jj in range(len(buttons)):
-            if (1<<jj & ii):
-                print("yep",jj,buttons[jj],tst,end=" ")
-                tst = tst ^ buttons[jj]
-                print(tst)
-                cnt +=1
-                if tst == light:
-                    print("cnt is ",cnt)
-                    if cnt < mincnt:
-                        mincnt = cnt
-           
-    mincnts.append(mincnt) 
-
-print("sum mincnts ", sum(mincnts))
-import pdb
-pdb.set_trace()
-mymat = np.array(myvals)
-pprint.pprint(mymat)
-
-
-N = mymat.shape[0]
-
-lookup = {}
-
-mindist = 99999900009
-for ii in range(N):
-    for kk in range(ii+1,N):
-        dif = mymat[ii] - mymat[kk]
-        area = dif[0] * dif[1]
-        lookup[area] = (ii,kk) 
-#        if dist < mindist:
-#            mindist = dist
-#            minii= ii
-#            minkk=kk
-
-skeys = sorted(lookup.keys())
-a,b = lookup[skeys[-1]]
-print(mymat[a],mymat[b])
-diff = mymat[a] - mymat[b]
-print((diff[0]+1) * (diff[1]+1))
-import pdb
-pdb.set_trace()
-
-sets = [set(),]
-sets[0].add(lookup[skeys[0]][0])
-sets[0].add(lookup[skeys[0]][1])
-
-
-G=nx.Graph()
-for ii in range(N):
-    G.add_node(ii)
-
-print(sets)
-#for ii in range(1,10):
-for ii in range(1,1000):
-    a,b = lookup[skeys[ii]]
-    G.add_edge(a,b)
-    print("adding",a,b)
-    if G.is_connected():
-        break
-        import pdb
-        pdb.set_trace()
-
-
-
-lens = []
-for stuff in nx.connected_components(G):
-    lens.append(len(stuff))
-print(lens)
-lens.sort()
-print(lens[-3:])
-print("prod is",np.prod(lens[-3:]))
-
-
-print(G)
-   
-if False:    
-    matcha = False
-    matchb = False
-    for group in sets:
-        if a in group:
-            group.add(b)
-            matcha = True
-            groupa = group
-    if match:
-        for group in sets:
-            if a in group:
-                groupa.update(group)
-                group = set()
-           
-    if not match:
-        sets.append(set((a,b))) 
-pprint.pprint(sets)
-
-print(mindist)
-print(minii,mymat[minii])
-print(minkk,mymat[minkk])
-
-
-
